3ab05fdb0a96ac7f95b0b9dff365b72cdc90f4f6
Anh Duy <nguyenlenhduy9412@gmail.com>
2026-01-18 04:45:28 +0700

    Simplify ALLOWED_HOSTS to wildcard

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,14 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '*',
-    'ibm-fullstack-developer-capstone.onrender.com',
-    'nelsonmsk710-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
-    'proxy.cognitiveclass.ai',
-    'nelsonmsk710-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai'
+    '*'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://ibm-fullstack-developer-capstone.onrender.com',
