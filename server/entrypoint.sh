3c0b9df9e09ee79f0b73d3558499aa7dd483d0d8
Anh Duy <nguyenlenhduy9412@gmail.com>
2026-01-18 04:48:40 +0700

    Add React build to entrypoint and create render.yaml for build configuration

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Build React frontend
+echo "Building React frontend..."
+cd frontend
+npm install
+npm run build
+cd ..
+
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
